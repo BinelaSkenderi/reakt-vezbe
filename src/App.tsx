0598bbe2c42e81d4df600c98fb5d97083348bb8c
Binela <binela.skenderi@outlook.com>
2025-02-28 00:04:55 +0100

    har testat useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef, use  } from 'react';
 import './App.css'
 
 function App() {
 const [number, setNumber]=useState(0); //vrednost, funkcija koj amenja vrednost
 const [name, setName] = useState("");
-
+const referenbcanaElement = useRef(null);
 
 function povecajBroj(){
+  referenbcanaElement.current.focus();//da udje u input/curent refencuje trenutni referenc
   setNumber(number + 1);
 }
 
@@ -15,17 +16,23 @@ function promeniIme(ev){
 }
 
 useEffect (()=>{
-  console.log(number);
-}, [number])
+  window.scrollTo({ behavior: "smooth", 
+    top: referenbcanaElement.current.offsetTop,
+  });//da baca odma na dole
+}, [])
 
   return (
-  <>
-  <button onClick={povecajBroj}>
-    Klikni me bre!
-  </button>
-  <input type="text" value={name} onChange={promeniIme}/><br />
+  <div>
+    {""}
+    <div style={{height: "800px"}}></div>
+    <div style={{height:"800px"}}>
+  <button onClick={povecajBroj}>Klikni me bre!</button>
+  <input ref={referenbcanaElement}
+   type="text" value={name} onChange={promeniIme}/>{""}
+   <br />
   {number}-{name}
-  </>
+  </div>
+  </div>
   );
 }
 
