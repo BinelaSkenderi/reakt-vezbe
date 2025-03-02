b919df2f1413abad1f9dfed776a8b1f6411d4875
Binela <binela.skenderi@outlook.com>
2025-03-02 14:43:37 +0100

    instalerat axios libary och ändrade lite function med Api

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,73 @@
 import { useState, useEffect, useRef, use  } from 'react';
 import './App.css'
+import axios from 'axios';
 
 function App() {
-const [number, setNumber]=useState(0); //vrednost, funkcija koj amenja vrednost
-const [name, setName] = useState("");
-const referenbcanaElement = useRef(null);
+  const instance = axios.create({
+    baseURL: 'https://catfact.ninja',
+    
+  });
+// const [number, setNumber]=useState(0); //vrednost, funkcija koj amenja vrednost
+// const [name, setName] = useState("");
+// const referenbcanaElement = useRef(null);
+const [fact, setFact] = useState(null);
+const [error, setError]= useState(null);
 
-function povecajBroj(){
-  referenbcanaElement.current.focus();//da udje u input/curent refencuje trenutni referenc
-  setNumber(number + 1);
-}
+// function povecajBroj( ){
+//   referenbcanaElement.current.focus();//da udje u input/curent refencuje trenutni referenc
+//   setNumber(number + 1); 
+// }
 
-function promeniIme(ev){
-  setName(ev.target.value); //na svaku promenu texta promeni varjablu ime
+// function promeniIme(ev){
+//   setName(ev.target.value); //na svaku promenu texta promeni varjablu ime
+// }
+
+async function getFact() {
+  try {
+    const response = await instance.get('/fact');
+    
+    if(response.data){
+      setFact(response.data.fact)
+      setError(null);
+    }
+    
+  } catch (error) {
+   
+    if(error){
+      setError(error.message);
+      setFact(null);
+    }
+  }
 }
 
 useEffect (()=>{
-  window.scrollTo({ behavior: "smooth", 
-    top: referenbcanaElement.current.offsetTop,
-  });//da baca odma na dole
-}, [])
+  // window.scrollTo({ 
+  //   behavior: "smooth", 
+  //   top: referenbcanaElement.current.offsetTop,
+  // });//da baca odma na dole
+  getFact();
+}, []);
+
+// if(fact){
+//   return fact
+// }
+//  else {
+//   return "loading..."
+//  }
+
 
   return (
   <div>
-    {""}
-    <div style={{height: "800px"}}></div>
-    <div style={{height:"800px"}}>
-  <button onClick={povecajBroj}>Klikni me bre!</button>
+  {/* <button onClick={povecajBroj}>Klikni me bre!</button>
   <input ref={referenbcanaElement}
    type="text" value={name} onChange={promeniIme}/>{""}
-   <br />
-  {number}-{name}
-  </div>
+   <br /> */}
+  {/* {fact ? fact : "loading... " }  */}
+  {fact && <p>{fact}</p>}
+  {error && <p>{error}</p>}
+  {!fact && !error && <p>Loading...</p>}
   </div>
   );
-}
+};
 
 export default App;
