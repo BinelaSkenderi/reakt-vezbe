1580cbd9fcd52589c314ef2ef83887216929e836
Binela <binela.skenderi@outlook.com>
2025-03-06 15:02:37 +0100

    har skappat button

--- src/components/page/home.tsx
+++ src/components/page/home.tsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
+import Button from "../ui/button";
+
 function Home(){
 
-    return <p>Home page</p>
+const [textButton, setTextButton] = useState("");   
+const onClick = (text: string) => {
+    setTextButton(text)
+}
+    return (
+        <>
+            <p>({textButton.length === 0 ? "Nisi kliknu button" : textButton})</p>
+            <Button text={"Click me"} tip={"error"} onClickButton={onClick}/>
+            <Button text={"klikni i mene"} tip={"default"} onClickButton={onClick}/>
+        </>
+    )
 };
 
 export default Home;
---
+++ src/components/ui/button.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface ButtonProps {
+  tip: string;
+  onClickButton: (text: string) => void;
+  text: string;
+}
+
+const Button = ({ tip, onClickButton, text }: ButtonProps) => {
+  console.log(tip);
+
+  return (
+    <button 
+      style={{ 
+        color: tip === "error" ? "red" : "unset", 
+        backgroundColor: tip === "error" ? "black" : "unset",
+      }}
+      onClick={() => onClickButton(text)}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
